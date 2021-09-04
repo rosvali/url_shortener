0d15cbd40d44de18d8fd6fe45a57d27ba21045d3
rosvali <rosval6578@gmail.com>
2021-09-04 17:41:51 +0200

    create url model

---
+++ app/models/url.rb
@@ -0,0 +1,16 @@
+require 'securerandom'
+
+class Url < ApplicationRecord
+	validates :original_url, presence: true
+
+	def custom_url
+		"shorturl.com/#{id_url}" if id_url
+	end
+
+	def generate_id_url
+		loop do
+			id_url = SecureRandom.alphanumeric(6)
+		break id_url unless Url.where(id_url: id_url).first
+		end
+	end
+end
